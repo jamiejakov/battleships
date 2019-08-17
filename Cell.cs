02d580d2f1f9171084a465de84cb653e3f520422
Vadims Brodskis <vadims.brodskis@smash.org.au>
2019-08-17 14:30:19 +1000

    preventing cell from being hit multiple times

--- Cell.cs
+++ Cell.cs
@@ -1,16 +1,19 @@
 public class Cell
 {
-    public Cell(char column, int row)
-    {
-        this.Row = row;
-        this.Column = column;
-        this.Key = "" + column + row;
-        this.Ship = null;
-    }
+  public Cell(char column, int row)
+  {
+    this.Row = row;
+    this.Column = column;
+    this.Key = "" + column + row;
+    this.Ship = null;
+    this.IsHit = false;
+  }
 
-    public int Row;
-    public char Column;
-    public string Key;
+  public int Row;
+  public char Column;
+  public string Key;
 
-    public Ship Ship;
+  public Ship Ship;
+
+  public bool IsHit;
 }
